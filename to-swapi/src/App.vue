d16fad529c647eee3c4f88067a2fc453ea28ffe8
Evan BOISSONNOT <evan@boissonnot.fr>
2021-05-25 17:41:20 +0200

    feat: trying to use v-for

---
+++ to-swapi/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld title="Swapi" subTitle="People list"/>
+  <PeopleList></PeopleList>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import PeopleList from './components/people-list.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    PeopleList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ to-swapi/src/components/people-list.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1>Star wars people</h1>
+
+  <ul>
+    <li v-for="item in getPeople" :key="item.id">
+      {{ item.id }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'PeopleList',
+  props: {
+  },
+  computed: {
+    getPeople: function () {
+      return [
+        { id: 1, name: 'coucou' }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
